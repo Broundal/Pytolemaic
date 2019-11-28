43debac8801a1554f76c7b9af8d98c57c63c49e0
Orion <orion@bidalgo.com>
2019-11-28 01:30:48 -0800

    linear dataset

---
+++ examples/datasets/linear.py
@@ -0,0 +1,69 @@
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
+import numpy
+
+from pytolemaic.utils.general import GeneralUtils
+
+class LinearBase():
+    def __init__(self, model, n_samples=500, n_features=3):
+        self.n_samples = n_samples
+        self.n_features = n_features
+
+        self._xtrain, self._ytrain = None, None
+        self.model = model
+
+
+    def _function(self, x):
+        y = 0 * x[:, 0] + 3 * x[:, 1] + numpy.sum(x[:, 2:], axis=1)
+        if self.y_type == float:
+            return y
+        else:
+            return y.astype(int)
+
+    def column_names(self):
+        # 1st has no importance, while 3rd has double importance
+
+        return ['zero importance', 'triple importance'] + ['regular importance #%d' % (k + 2) for k in
+                                                                    range(self.n_features - 2)]
+
+    @property
+    def train_data(self):
+        if self._xtrain is None:
+            self._xtrain, self._ytrain = self.get_samples()
+
+        return self._xtrain, self._ytrain
+
+    def get_samples(self):
+        # Let's define dataset and label
+
+        x = numpy.random.rand(self.n_samples, self.n_features)
+
+        y = self._function(x)
+
+        x = GeneralUtils.add_nans(x)
+
+        return x, y
+
+    def get_model(self):
+        x, y = self.train_data
+        self.model.fit(x, y)
+        return self.model
+
+class LinearClassificationDataset(LinearBase):
+    def __init__(self, **kwargs):
+        super(LinearClassificationDataset, self).__init__(
+            model=GeneralUtils.simple_imputation_pipeline(
+                RandomForestClassifier(random_state=0, n_estimators=3)),
+            **kwargs)
+        self.y_type = int
+
+class LinearRegressionDataset(LinearBase):
+    def __init__(self, **kwargs):
+        super(LinearRegressionDataset, self).__init__(
+            model=GeneralUtils.simple_imputation_pipeline(
+                RandomForestRegressor(random_state=0, n_estimators=3)),
+            **kwargs)
+        self.y_type = float
+
+
+
--- examples/example_prediction_uncertainty.py
+++ examples/example_prediction_uncertainty.py
@@ -1,6 +1,7 @@
 import numpy
 from sklearn.ensemble import RandomForestClassifier
 
+from examples.datasets.linear import LinearClassificationDataset
 from pytolemaic.pytrust import PyTrust
 from pytolemaic.utils.dmd import DMD
 from pytolemaic.utils.general import GeneralUtils
@@ -8,27 +9,13 @@ from pytolemaic.utils.metrics import Metrics
 
 
 def run():
-    # Let's define dataset and label
 
-    xtrain = numpy.random.rand(10000, 3)
-    columns_names = ['zero importance', 'regular importance', 'triple importance']
+    # Obtain simple classification dataset. Use LinearRegressionDataset for regression
+    dataset = LinearClassificationDataset()
 
-    # 1st has no importance, while 3rd has double importance
-    ytrain = 0 * xtrain[:, 0] + 1 * xtrain[:, 1] + 3 * xtrain[:, 2]
-    ytrain = ytrain.astype(int)
-
-    xtest = numpy.random.rand(10000, 3)
-    # 1st has no importance, while 3rd has double importance
-    ytest = 0 * xtest[:, 0] + 1 * xtest[:, 1] + 3 * xtest[:, 2]
-    ytest = ytest.astype(int)
-
-    xtrain = GeneralUtils.add_nans(xtrain)
-    xtest = GeneralUtils.add_nans(xtest)
-
-    ## Let's train a classifier
-    classifier = GeneralUtils.simple_imputation_pipeline(
-        RandomForestClassifier(random_state=0, n_estimators=3))
-    classifier.fit(xtrain, ytrain.ravel())
+    # uncertainty model requires test data and trained model
+    xtest, ytest = dataset.get_samples()
+    classifier = dataset.get_model()
 
     ## set metric
     metric = Metrics.recall
@@ -38,39 +25,38 @@ def run():
         xtest=xtest, ytest=ytest,
         metric=metric)
 
-    # method = 'confidence'
-    method = 'probability'
-    uncertainty_model = pytrust.create_uncertainty_model(method=method)
+    # uncertainty model may be based on 'confidence' or 'probability' for classification, and 'mae' or 'rmse' for regression
+    for method in ['confidence', 'probability']:
+        uncertainty_model = pytrust.create_uncertainty_model(method=method)
 
-    # create another test set, this time to test uncertainty
-    x_new_test = numpy.random.rand(10000, 3)
-    y_new_test = 0 * x_new_test[:, 0] + 1 * x_new_test[:, 1] + 3 * x_new_test[:, 2]
-    y_new_test = y_new_test.astype(int)
+        # create another test set, this time to test uncertainty
+        x_new_test, y_new_test = dataset.get_samples()
 
-    new_test = DMD(x=x_new_test, y=y_new_test)
+        new_test = DMD(x=x_new_test, y=y_new_test)
 
-    yp = uncertainty_model.predict(new_test)  # this is same as model.predict
-    base_score = metric.function(new_test.target, yp)
+        yp = uncertainty_model.predict(new_test)  # this is same as model.predict
+        base_score = metric.function(new_test.target, yp)
 
-    uncertainty = uncertainty_model.uncertainty(new_test)
-    p25, p50, p75 = numpy.percentile(numpy.unique(uncertainty), [25, 50, 75])
+        uncertainty = uncertainty_model.uncertainty(new_test)
+        p25, p50, p75 = numpy.percentile(numpy.unique(uncertainty), [25, 50, 75])
 
-    # samples with low uncertainty
-    good = (uncertainty < p25).ravel()
-    subset_good_score = metric.function(
-        y_true=y_new_test[good], y_pred=yp[good])
+        # samples with low uncertainty
+        good = (uncertainty < p25).ravel()
+        subset_good_score = metric.function(
+            y_true=y_new_test[good], y_pred=yp[good])
 
-    # samples with high uncertainty
-    bad = (uncertainty > p75).ravel()
-    subset_bad_score = metric.function(
-        y_true=y_new_test[bad], y_pred=yp[bad])
+        # samples with high uncertainty
+        bad = (uncertainty > p75).ravel()
+        subset_bad_score = metric.function(
+            y_true=y_new_test[bad], y_pred=yp[bad])
 
-    print("performance for method {}".format(method))
-    print('{} score is {:0.3f}'.format(metric.name, base_score))
-    print('{} score for samples with high confidence is {:0.3f}'.format(metric.name, subset_good_score))
-    print('{} score for samples with low confidence is {:0.3f}'.format(metric.name, subset_bad_score))
-    print('{:0.3f} < {:0.3f} < {:0.3f} = {}'.format(subset_bad_score, base_score, subset_good_score,
-                                                    subset_bad_score < base_score < subset_good_score))
+        print('\n\n\n#########################################333\n')
+        print("performance for method *{}*".format(method))
+        print('{} score is {:0.3f}'.format(metric.name, base_score))
+        print('{} score for samples with high confidence is {:0.3f}'.format(metric.name, subset_good_score))
+        print('{} score for samples with low confidence is {:0.3f}'.format(metric.name, subset_bad_score))
+        print('{:0.3f} < {:0.3f} < {:0.3f} = {}'.format(subset_bad_score, base_score, subset_good_score,
+                                                        subset_bad_score < base_score < subset_good_score))
 
 
 if __name__ == '__main__':
